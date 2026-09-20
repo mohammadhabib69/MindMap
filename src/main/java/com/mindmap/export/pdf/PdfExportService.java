c63b72792e6c5c094fe7a4be9dd2d25bae19a10c
Mohammad Habib <mohammadhabib42730@gmail.com>
2026-09-20 16:15:31 +0600

    Fix PDF export font rendering with FreeSerif

--- src/main/java/com/mindmap/export/pdf/PdfExportService.java
+++ src/main/java/com/mindmap/export/pdf/PdfExportService.java
@@ -41,15 +41,15 @@ public class PdfExportService {
 
     private void initFonts() {
         try {
-            byte[] fontBytes = loadFontBytes("/fonts/NotoSansBengali-Regular.ttf");
+            byte[] fontBytes = loadFontBytes("/fonts/FreeSerif.ttf");
             if (fontBytes != null) {
-                BaseFont baseFont = BaseFont.createFont("NotoSansBengali-Regular.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, true, fontBytes, null);
+                BaseFont baseFont = BaseFont.createFont("FreeSerif.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED, true, fontBytes, null);
                 titleFont = new Font(baseFont, 18, Font.BOLD);
                 subtitleFont = new Font(baseFont, 14, Font.BOLD);
                 normalFont = new Font(baseFont, 11, Font.NORMAL);
                 metadataFont = new Font(baseFont, 10, Font.ITALIC);
             } else {
-                LOGGER.warning("Could not load Bengali font, falling back to default.");
+                LOGGER.warning("Could not load Unicode font, falling back to default.");
                 titleFont = new Font(Font.HELVETICA, 18, Font.BOLD);
                 subtitleFont = new Font(Font.HELVETICA, 14, Font.BOLD);
                 normalFont = new Font(Font.HELVETICA, 11, Font.NORMAL);
